b4dcce8fcfe886b00e571705f762ca4d12df2bec
Frank Morgan <johnfrankmorgan@gmail.com>
2024-08-22 17:14:35 +0100

    add some more io functions (#2)

--- io.go
+++ io.go
@@ -15,3 +15,13 @@ type _io struct {
 func (_io) Close(closer io.Closer) {
 	NotError(closer.Close())
 }
+
+// Copy can be used to panic if a call to [io.Copy] fails.
+func (_io) Copy(dst io.Writer, src io.Reader) int64 {
+	return Succeed(io.Copy(dst, src))
+}
+
+// ReadAll can be used to panic if a call to [io.ReadAll] fails.
+func (_io) ReadAll(r io.Reader) []byte {
+	return Succeed(io.ReadAll(r))
+}
