4aadd797c84546292a7b49495628b61835837801
johnfrankmorgan <johnfrankmorgan@gmail.com>
2024-08-22 17:07:50 +0100

    add some more io functions

--- io_test.go
+++ io_test.go
@@ -1,8 +1,10 @@
 package must_test
 
 import (
+	"bytes"
 	"errors"
 	"testing"
+	"testing/iotest"
 
 	"github.com/johnfrankmorgan/must"
 	"github.com/stretchr/testify/assert"
@@ -41,4 +43,46 @@ func TestIOClose_NoError_DoesNotPanic(t *testing.T) {
 	})
 
 	assert.True(t, c.called)
+}
+
+func TestIOCopy_Error_Panics(t *testing.T) {
+	t.Parallel()
+
+	src := iotest.ErrReader(errors.New("Copy"))
+
+	assert.PanicsWithError(t, "must: unexpected error: Copy", func() {
+		must.IO.Copy(&bytes.Buffer{}, src)
+	})
+}
+
+func TestIOCopy_NoError_CopiesBytes(t *testing.T) {
+	t.Parallel()
+
+	src := bytes.NewBufferString("Hello, World!")
+	dst := bytes.NewBuffer(nil)
+
+	n := must.IO.Copy(dst, src)
+
+	assert.Equal(t, "Hello, World!", dst.String())
+	assert.EqualValues(t, 13, n)
+}
+
+func TestIOReadAll_Error_Panics(t *testing.T) {
+	t.Parallel()
+
+	src := iotest.ErrReader(errors.New("ReadAll"))
+
+	assert.PanicsWithError(t, "must: unexpected error: ReadAll", func() {
+		must.IO.ReadAll(src)
+	})
+}
+
+func TestIOReadAll_NoError_ReadsData(t *testing.T) {
+	t.Parallel()
+
+	src := bytes.NewBufferString("Hello, World!")
+
+	res := must.IO.ReadAll(src)
+
+	assert.Equal(t, "Hello, World!", string(res))
 }
