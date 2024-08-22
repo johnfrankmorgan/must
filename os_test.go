25b34b837a209aa0a3508d6539d5d0631b8207b5
Frank Morgan <johnfrankmorgan@gmail.com>
2024-08-22 17:00:42 +0100

    namespace io and os functions (#1)

--- os_test.go
+++ os_test.go
@@ -11,15 +11,15 @@ func TestOpen_Error_Panics(t *testing.T) {
 	t.Parallel()
 
 	assert.PanicsWithError(t, "must: unexpected error: open does-not-exist: no such file or directory", func() {
-		must.Open("does-not-exist")
+		must.OS.Open("does-not-exist")
 	})
 }
 
 func TestOpen_NoError_ReturnsFile(t *testing.T) {
 	t.Parallel()
 
-	f := must.Open("os.go")
-	defer must.Close(f)
+	f := must.OS.Open("os.go")
+	defer must.IO.Close(f)
 
 	assert.NotNil(t, f)
 	assert.Equal(t, "os.go", f.Name())
@@ -29,16 +29,16 @@ func TestCreate_Error_Panics(t *testing.T) {
 	t.Parallel()
 
 	assert.PanicsWithError(t, "must: unexpected error: open /not-writable: permission denied", func() {
-		must.Create("/not-writable")
+		must.OS.Create("/not-writable")
 	})
 }
 
 func TestCreate_NoError_ReturnsFile(t *testing.T) {
 	t.Parallel()
 
-	f := must.Create("tmp/Create.txt")
-	defer must.Close(f)
-	defer must.Remove(f.Name())
+	f := must.OS.Create("tmp/Create.txt")
+	defer must.IO.Close(f)
+	defer must.OS.Remove(f.Name())
 
 	assert.NotNil(t, f)
 	assert.Equal(t, "tmp/Create.txt", f.Name())
@@ -48,17 +48,17 @@ func TestRemove_Error_Panics(t *testing.T) {
 	t.Parallel()
 
 	assert.PanicsWithError(t, "must: unexpected error: remove does-not-exist: no such file or directory", func() {
-		must.Remove("does-not-exist")
+		must.OS.Remove("does-not-exist")
 	})
 }
 
 func TestRemove_NoError_DoesNotPanic(t *testing.T) {
 	t.Parallel()
 
-	f := must.Create("tmp/Remove.txt")
-	must.Close(f)
+	f := must.OS.Create("tmp/Remove.txt")
+	must.IO.Close(f)
 
 	assert.NotPanics(t, func() {
-		must.Remove("tmp/Remove.txt")
+		must.OS.Remove("tmp/Remove.txt")
 	})
 }
